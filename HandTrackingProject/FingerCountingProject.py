9b5830ab6546591af7273f8cb964f0a7e797c5bb
JigneshBotla <159447423+JigneshBotla@users.noreply.github.com>
2024-12-09 21:40:13 +0530

    Finger Counting

---
+++ HandTrackingProject/FingerCountingProject.py
@@ -0,0 +1,66 @@
+import cv2
+import time
+import os
+import HandTrackingModule as htm
+
+cap = cv2.VideoCapture(0)
+wCam, hCam = 640, 480
+cap.set(3, wCam)
+cap.set(4, hCam)  
+folderPath = r"C:\Users\botla\OneDrive\Desktop\Computer vision\HandTrackingProject\FingerImages"
+myList = os.listdir(folderPath)
+
+overlayList = []
+
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    # Resize image to a smaller size
+    image = cv2.resize(image, (200, 200))
+    overlayList.append(image)
+
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.75)
+
+tipIds = [4, 8, 12, 16, 20]
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    
+    if len(lmList) != 0:
+        fingers = []
+        # Thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        # 4 Fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+        
+        # Place small overlay in top-left corner
+        h, w, _ = overlayList[totalFingers - 1].shape
+        img[10:10+h, 10:10+w] = overlayList[totalFingers - 1]
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
+                    10, (255, 0, 0), 25)
+    
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)
+    cv2.imshow("Image", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
